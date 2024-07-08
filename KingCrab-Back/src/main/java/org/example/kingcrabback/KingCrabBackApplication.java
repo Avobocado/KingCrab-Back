e0ad1a730a209f1e1e40937c628699f92d2cb6ae
ByunDohwi <dohwe0978@gmail.com>
2024-07-08 20:14:32 +0900

    create :: 프로젝트 생성

---
+++ KingCrab-Back/src/main/java/org/example/kingcrabback/KingCrabBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.kingcrabback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingCrabBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingCrabBackApplication.class, args);
+    }
+
+}
---
+++ KingCrab-Back/src/test/java/org/example/kingcrabback/KingCrabBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kingcrabback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingCrabBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
