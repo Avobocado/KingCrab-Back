36cf6af11b87c96968b1e37e22aa3d6c23bcf8cc
ByunDohwi <dohwe0978@gmail.com>
2024-07-09 20:29:23 +0900

    add :: yml

--- src/main/java/org/example/kingcrabback/domain/comment/controller/CommentController.java
+++ src/main/java/org/example/kingcrabback/domain/comment/controller/CommentController.java
@@ -1,4 +1,8 @@
 package org.example.kingcrabback.domain.comment.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
 }
