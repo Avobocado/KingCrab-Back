d50b9c24f3ef9d474a949b1d06991a3a49dba4ff
imhanseong <hans_hanseong@dsm.hs.kr>
2024-07-22 16:22:59 +0900

    add :: post 전체 조회 생성

--- src/main/java/org/example/kingcrabback/domain/post/controller/PostController.java
+++ src/main/java/org/example/kingcrabback/domain/post/controller/PostController.java
@@ -1,11 +1,15 @@
 package org.example.kingcrabback.domain.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.kingcrabback.domain.post.dto.PostListResponse;
 import org.example.kingcrabback.domain.post.dto.PostRequest;
+import org.example.kingcrabback.domain.post.service.PostReadAllService;
 import org.example.kingcrabback.domain.post.service.PostCreateService;
 import org.example.kingcrabback.domain.post.service.PostDeleteService;
 import org.example.kingcrabback.domain.post.service.PostUpdateService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -14,6 +18,7 @@ public class PostController {
     private final PostCreateService postCreateService;
     private final PostDeleteService postDeleteService;
     private final PostUpdateService postUpdateService;
+    private final PostReadAllService postAllReadService;
 
     @PostMapping("/create")
     public void createPost(@RequestBody PostRequest postRequest){
@@ -28,5 +33,10 @@ public class PostController {
     @PatchMapping("{id}")
     public void updatePost(@PathVariable Long id, @RequestBody PostRequest postRequest){
         postUpdateService.updatePost(id, postRequest);
+    }
+
+    @GetMapping()
+    public List<PostListResponse> readAllPost(){
+        return postAllReadService.readAllPost();
     }
 }
--- src/main/java/org/example/kingcrabback/domain/post/repository/PostRepository.java
+++ src/main/java/org/example/kingcrabback/domain/post/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package org.example.kingcrabback.domain.post.repository;
 
 import org.example.kingcrabback.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PostRepository extends CrudRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findAllById (Long id);
 }
