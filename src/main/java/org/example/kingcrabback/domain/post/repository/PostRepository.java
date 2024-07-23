c7faf612e0c0ea287ecb75424522666fbcec062e
imhanseong <hans_hanseong@dsm.hs.kr>
2024-07-23 23:10:48 +0900

    change :: post repository 수정

--- src/main/java/org/example/kingcrabback/domain/post/repository/PostRepository.java
+++ src/main/java/org/example/kingcrabback/domain/post/repository/PostRepository.java
@@ -5,14 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
-    List<Post> findAllById (Long id);
-=======
-@Repository
-public interface PostRepository extends CrudRepository<Post,Long> {
->>>>>>> origin/post
+    List<Post> findAllById(Long id);
 }
