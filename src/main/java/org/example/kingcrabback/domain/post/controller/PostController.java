e12df6a4a61048512d50ff0b082468d7dd1edb7a
imhanseong <hans_hanseong@dsm.hs.kr>
2024-07-23 22:12:29 +0900

    change :: post controller 수정

--- src/main/java/org/example/kingcrabback/domain/post/controller/PostController.java
+++ src/main/java/org/example/kingcrabback/domain/post/controller/PostController.java
@@ -1,23 +1,16 @@
 package org.example.kingcrabback.domain.post.controller;
 
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
 import org.example.kingcrabback.domain.post.dto.PostListResponse;
 import org.example.kingcrabback.domain.post.dto.PostRequest;
 import org.example.kingcrabback.domain.post.service.PostReadAllService;
-=======
-import org.example.kingcrabback.domain.post.dto.PostRequest;
->>>>>>> origin/post
 import org.example.kingcrabback.domain.post.service.PostCreateService;
 import org.example.kingcrabback.domain.post.service.PostDeleteService;
 import org.example.kingcrabback.domain.post.service.PostUpdateService;
 import org.springframework.web.bind.annotation.*;
 
-<<<<<<< HEAD
 import java.util.List;
 
-=======
->>>>>>> origin/post
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
@@ -25,10 +18,7 @@ public class PostController {
     private final PostCreateService postCreateService;
     private final PostDeleteService postDeleteService;
     private final PostUpdateService postUpdateService;
-<<<<<<< HEAD
     private final PostReadAllService postAllReadService;
-=======
->>>>>>> origin/post
 
     @PostMapping("/create")
     public void createPost(@RequestBody PostRequest postRequest){
@@ -44,12 +34,9 @@ public class PostController {
     public void updatePost(@PathVariable Long id, @RequestBody PostRequest postRequest){
         postUpdateService.updatePost(id, postRequest);
     }
-<<<<<<< HEAD
 
     @GetMapping()
     public List<PostListResponse> readAllPost(){
         return postAllReadService.readAllPost();
     }
-=======
->>>>>>> origin/post
 }
