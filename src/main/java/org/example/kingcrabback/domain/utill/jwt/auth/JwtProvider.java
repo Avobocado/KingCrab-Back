caeffa2f3f3e3a2b35650c29e8136ebd95d89211
ByunDohwi <dohwe0978@gmail.com>
2024-07-29 23:19:47 +0900

    add :: utill

---
+++ src/main/java/org/example/kingcrabback/domain/utill/jwt/auth/JwtProvider.java
@@ -0,0 +1,9 @@
+package org.example.kingcrabback.domain.utill.jwt.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+}
